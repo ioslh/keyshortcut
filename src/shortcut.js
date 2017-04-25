12c218b3074e833dcc62e798b88b48c98fbf6c05
ioslh <ohislh@gmail.com>
2017-04-26 00:44:52 +0800

    update:version

--- src/shortcut.js
+++ src/shortcut.js
@@ -1,10 +1,14 @@
-(function( factory ){
-    if (typeof exports !== 'undefined') {
-        exports.default = factory( {} );
-    } else if(typeof window !== 'undefined') {
-        window.KeyShortcut = factory( {} );
-    }
-})(function( KeyShortcut ){
+(function( root, factory ){
+
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory({}});
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["KeyShortcut"] = factory({});
+	else
+		root["KeyShortcut"] = factory({});
+})(this, function( KeyShortcut ){
 
     var tmpKey,tmpValue;
     var hasOwn = function( obj, key ){
@@ -56,7 +60,7 @@
     KeyShortcut.DEBUG = false;
     // 是否允许连击触发，即按住某一个按键不放。
     KeyShortcut.repeat = true;
-    KeyShortcut.VERSION = '0.0.4';
+    KeyShortcut.VERSION = '0.0.5';
     KeyShortcut.timeout = 0;
     KeyShortcut.register = function( key, callback, context ){
         if( typeof key === 'string' ){
